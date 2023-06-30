ed599829287fd6f95b153ab902a404926e85aa97
Arthur Kafrouni Tomazi <arthur.k.t@hotmail.com>
2023-06-30 16:47:55 -0300

    Add files via upload

---
+++ lista1.1.8.py
@@ -0,0 +1,28 @@
+"""
+Lista 1, parte 1, exercício 8
+
+Descrição: Este programa apresenta a resolução da questão 8 da parte 1 da lista 1 da cadeira de Economia Computacional
+Autor:Arthur Kafrouni Tomazi
+Data: 23/06/2023
+Versão: 0.0.1
+"""
+
+# Problema: Resolva a equação linear a.x = b
+
+# Atribuição de variáveis
+
+a = 0
+x = 0
+
+# Entrada de dados
+
+a = float(input("a: "))
+x = float(input("x: "))
+
+# Processamento de dados
+
+b = a*x
+
+# Saída de dados
+
+print(f'Quando a for igual "a" {a} e "x" for igual a {x}, "b" será igual a {b}')
